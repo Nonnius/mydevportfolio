b0a82055c8b5bd1bf28becd9cdb95196819abc3f
Nonnius <omarr.reid@outlook.com>
2025-11-13 07:59:58 -0500

    Update deployed app

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -32,7 +32,8 @@ const Skills = () => {
       <div className="skillsCard 
       s24Ultra1:mt-[4rem]
       mt-[-.8rem]
-      flex-1 flex flex-col-2
+      grid grid-cols-2 
+      s24Ultra1:grid-cols-1
       gap-4 min-h-[60px] max-w-[65rem] mx-auto p-6 rounded-lg">
         <div className="w-full bg-white/15 rounded border-4 border-[#04ff00] backdrop-blur-md py-2 px-4 text-[#04ff00]
         text-center">
@@ -107,41 +108,41 @@ const Skills = () => {
               <span className='text-[14px]'>Axios</span>            
             </div>
           </div>
-        </div>       
-      </div>
-      <div className="toolCard
-      s24Ultra1:mt-[-1.8rem]
-      mt-[-2rem]
-      flex flex-wrap justify-center gap-4 min-h-[65px] p-6 max-w-[65rem] mx-auto rounded-lg">       
+        </div>
+        <div className="toolCard
+        s24Ultra1:mt-[-1.8rem]
+        mt-[-2rem] grid grid-cols-1 justify-center gap-4 min-h-[65px] p-6 max-w-[68rem] mx-auto rounded-lg">               
         <div className="bg-white/15 rounded border-4 border-[#04ff00] backdrop-blur-md py-1 text-[#04ff00]
-        text-center">
-          <p className='text-[#04ff00] mb-2 underline'>
-            Tools
-          </p>
-          <div className='flex flex-wrap gap-2 text-[16px] sm:items-center justify-center'>
-            <div className="flex items-center gap-1 px-2 py-1 rounded text-[#04ff00]">
-              <FaGitAlt className='text-orange-500 text-[24px]' />
-              <span className='text-[14px]'>Git</span>            
-            </div>
-            <div className="flex items-center gap-1 px-2 py-1 rounded text-[#04ff00]">
-              <VscVscode className='text-blue-400 text-[24px]' />
-              <span className='text-[14px]'>VSCode</span>            
-            </div>
-            <div className="flex items-center gap-1 px-2 py-1 rounded text-[#04ff00]">
-              <FaGithubSquare className='text-black text-[24px]' />
-              <span className='text-[14px]'>Github</span>            
-            </div>
-            <div className="flex items-center gap-1 px-2 py-1 rounded text-[#04ff00]">
-              <RiVercelFill className='text-black text-[24px]' />
-              <span className='text-[14px]'>Vercel</span>            
-            </div>
-            <div className="flex items-center gap-1 px-2 py-1 rounded text-[#04ff00]">
-              <SiPostman className='text-orange-500 text-[24px]' />
-              <span className='text-[14px]'>Postman</span>            
+          text-center">
+            <p className='text-[#04ff00] mb-2 underline'>
+              Tools
+            </p>
+            <div className='flex flex-wrap gap-2 text-[16px] sm:items-center justify-center'>
+              <div className="flex items-center gap-1 px-2 py-1 rounded text-[#04ff00]">
+                <FaGitAlt className='text-orange-500 text-[24px]' />
+                <span className='text-[14px]'>Git</span>            
+              </div>
+              <div className="flex items-center gap-1 px-2 py-1 rounded text-[#04ff00]">
+                <VscVscode className='text-blue-400 text-[24px]' />
+                <span className='text-[14px]'>VSCode</span>            
+              </div>
+              <div className="flex items-center gap-1 px-2 py-1 rounded text-[#04ff00]">
+                <FaGithubSquare className='text-black text-[24px]' />
+                <span className='text-[14px]'>Github</span>            
+              </div>
+              <div className="flex items-center gap-1 px-2 py-1 rounded text-[#04ff00]">
+                <RiVercelFill className='text-black text-[24px]' />
+                <span className='text-[14px]'>Vercel</span>            
+              </div>
+              <div className="flex items-center gap-1 px-2 py-1 rounded text-[#04ff00]">
+                <SiPostman className='text-orange-500 text-[24px]' />
+                <span className='text-[14px]'>Postman</span>            
+              </div>
             </div>
           </div>
-        </div>
+        </div>       
       </div>
+      
       
       
     </div>
